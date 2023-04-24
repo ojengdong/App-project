a4bd7949bb8affd17b176eda29ccc5881ea34fb8
ojengdong <http://github.com/ojengdong>
2023-04-24 18:56:45 +0900

    dddd

--- movie/src/App.js
+++ movie/src/App.js
@@ -4,17 +4,16 @@ import {useState, useEffect} from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
-  
+  const [movies, setMovies] = useState([])
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then(Response => 
-    Response.json())
-    .then((json) => setCoins(json))
-  }, [])
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+  },[])
+  
   return (
     <div className="App">
-      <h1>The Coins</h1>
-      {loading ? <strong>Loading...</strong> : null}
+      {loading ? <h1>Loading...</h1> : null}
     </div>
   );
 }
