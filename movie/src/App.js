07c4619fae0dc75f0dac53c256b41e12a12f63ec
ojengdong <http://github.com/ojengdong>
2023-04-24 18:56:52 +0900

    dddd

--- movie/src/App.js
+++ movie/src/App.js
@@ -5,15 +5,33 @@ import {useState, useEffect} from 'react';
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([])
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+      ).json();
+      setMovies(json.data.movies);
+      setLoading(false);
+  };
   useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-  },[])
-  
+    getMovies();
+  }, []);
+  console.log(movies)
   return (
     <div className="App">
-      {loading ? <h1>Loading...</h1> : null}
+      {loading ? <h1>Loading...</h1> : movies.map((movie) => (
+        <div key={movie.id}>
+          <img src={movie.medium_cover_image} alt="movie img" />
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map(g => 
+              (<li key={g}>{g}</li>)
+              )}
+          </ul>
+          </div>
+      ))}
     </div>
   );
 }
