1dcec2475e42bbc78cf195f9c3d6c3437f4a7f38
ojengdong <dutkdgus159@naver.com>
2023-05-26 01:29:24 +0900

    ddd

--- movie/src/routes/Detail.jsx
+++ movie/src/routes/Detail.jsx
@@ -1,57 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { Container, Row, Col, Spinner } from 'react-bootstrap'
-import Movie from '../Component/Movie'
-import './Detail.css'
-import Home from './Home'
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import Movie from "../Component/Movie";
+import "./Detail.css";
+import Home from "./Home";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { House } from "@fortawesome/free-solid-svg-icons";
 
-const Detail = (props) => {
-  const [movie,setMovie] =useState([])
-  const [loading,setLoading] = useState(true);
-  const {id} = useParams();
-    const getMovie = async () => {
-      const json = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
-      )
-    ).json()
-    setMovie(json.data.movie)
-    setLoading(false)
-  }
-  
-    useEffect(() => {
-        getMovie();
-    }, [])
-    console.log(movie)
+const Detail = () => {
+  const [movie, setMovie] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+    console.log(json.data.movie);
+  };
+  console.log(movie.summary);
+  console.log(movie.title);
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
   return loading ? (
-      <div className='loading-box'>
-        <h1 className='loading'>Loading...</h1>
-        <div className='animation'></div>
-      </div>
+    <div className="loading-box">
+      <h1 className="loading">Loading...</h1>
+      <div className="animation"></div>
+    </div>
   ) : (
-    <div className='Detail-box'>
-      <Link to={'/'}>
-      <div className='back'>X</div>
+    <div className="Detail-box">
+      <Link to={`${process.env.PUBLIC_URL}/`}>
+        <div className="back">X</div>
       </Link>
-      <div className='Detail-img-box'>
+      <div className="Detail-img-box">
         <img src={movie?.medium_cover_image} alt="" />
       </div>
-      <div className='Detail-movie-wrapper'>
-        <h2 className='Detail-title'>제목 : {movie?.title}</h2>
-        <ul className='Detail-genres-list'>
-          <li className="Detail-genres-item">
-            장르 : {movie?.genres.join(' ')}
-          </li>
+      <div className="Detail-movie-wrapper">
+        <h2 className="Detail-title">제목 : {movie?.title}</h2>
+        <ul className="Detail-genres-list">
+          <li className="Detail-genres-item">장르 : {movie?.genres}</li>
         </ul>
-        <div className='empty'></div>
-        <div className='good-year-time'>
+        <div className="empty"></div>
+        <div className="good-year-time">
           <strong>평점 : {movie?.rating}</strong>
           <p>개봉년도 : {movie?.year}</p>
-          <p>러닝타임 : {movie.runtime}</p>
+          <p>러닝타임 : {movie?.runtime}분</p>
+        </div>
+        <div className="sumary-box">
+          {console.log(movie)}
+          <p>줄거리 : {movie?.description_full}</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
--- movie/src/routes/Home.jsx
+++ movie/src/routes/Home.jsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import Movie from '../Component/Movie'
-import { useState, useEffect, useRef } from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react'
-import "swiper/css"
-import "swiper/css/pagination"
+import React from "react";
+import Movie from "../Component/Movie";
+import { useState, useEffect, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination, Navigation, EffectCoverflow, Mousewheel,Keyboard } from "swiper";
-import './Home.css'
-
+import {
+  Pagination,
+  Navigation,
+  EffectCoverflow,
+  Mousewheel,
+  Keyboard,
+} from "swiper";
+import "./Home.css";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([])
-    
-    const getMovies = async () => {
+  const getMovies = async () => {
     const json = await (
       await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
       )
-      ).json();
-      setMovies(json.data.movies);
-      setLoading(false);
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
-  console.log(movies)
+  console.log(movies);
 
   return (
-    
-    <div className='movie-home'>
+    <div className="movie-home">
       <Swiper
         keyboard={{
           enabled: true,
@@ -40,41 +43,44 @@ const Home = () => {
         centeredSlides={true}
         slidesPerView={4}
         coverflowEffect={{
-          rotate: 10, 
-          stretch: 0, 
-          depth: 100, 
-          modifier: 2,  
+          rotate: 10,
+          stretch: 0,
+          depth: 100,
+          modifier: 2,
           slideShadows: true,
         }}
-        navigation={true} 
-        mousewheel={true} 
-        modules={[EffectCoverflow,Navigation,Mousewheel,Keyboard]} 
+        navigation={true}
+        mousewheel={true}
+        modules={[EffectCoverflow, Navigation, Mousewheel, Keyboard]}
         className="mySwiper"
       >
-      <div className='movie-item'>
-        {loading ? 
-        <div className='loading-box'>
-          <h1 className='loading'>Loading...</h1> 
-          <div className='animation'></div> 
-        </div>  : movies.map((movie) => (
-          <SwiperSlide>
-        <Movie 
-          key={movie.id}
-          id={movie.id}
-          coverImg={movie.medium_cover_image} 
-          title={movie.title} 
-          summary={movie.summary} 
-          genres={movie.genres}
-          rating={movie.rating}
-          year={movie.year}
-          runtime={movie.runtime}
-        />
-        </SwiperSlide>
-          ))}
-      </div>
+        <div className="movie-item">
+          {loading ? (
+            <div className="loading-box">
+              <h1 className="loading">Loading...</h1>
+              <div className="animation"></div>
+            </div>
+          ) : (
+            movies.map((movie) => (
+              <SwiperSlide>
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  coverImg={movie.medium_cover_image}
+                  title={movie.title}
+                  summary={movie.summary}
+                  genres={movie.genres}
+                  rating={movie.rating}
+                  year={movie.year}
+                  runtime={movie.runtime}
+                />
+              </SwiperSlide>
+            ))
+          )}
+        </div>
       </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
